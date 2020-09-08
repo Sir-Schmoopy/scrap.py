8140d52001da30035f9341964bd5a96b2f5b6963
Sky Hays <sky@skys-mbp.attlocal.net>
2020-09-07 23:07:16 -0500

    golf

--- golf.py
+++ golf.py
@@ -3,7 +3,7 @@ import random
 list1 = ["Abby", "Aleisa", "Andrew", "Angie", "BenK", "BenN", "BenW", "Billy", "Brittany", "Caleb", "Corey", "Cyrus", "Dakota", "Des", "Garrett", "Jared", "Jed", "Joshua", "Lida", "Lindsey", "Marian", "Mathew", "Bergy", "Schmidty", "Nathan", "Paul", "Samuel", "Sarah", "Sean", "Sky", "Steve", "Taryn", "Tiera", "Tori"]
 list2 = ["Abby", "Aleisa", "Andrew", "Angie", "BenK", "BenN", "BenW", "Billy", "Brittany", "Caleb", "Corey", "Cyrus", "Dakota", "Des", "Garrett", "Jared", "Jed", "Joshua", "Lida", "Lindsey", "Marian", "Mathew", "Bergy", "Schmidty", "Nathan", "Paul", "Samuel", "Sarah", "Sean", "Sky", "Steve", "Taryn", "Tiera", "Tori"]
 
-for i in range(34):
+for i in range(len(list1)):
     mugger1 = random.choice(list1)
     mugger2 = random.choice(list2)
     if mugger1 != mugger2:
