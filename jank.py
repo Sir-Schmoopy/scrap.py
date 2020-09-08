1bf6d3c25fac5fd53046ed34c72df394cdc88dd5
Sky Hays <sky@skys-mbp.attlocal.net>
2020-09-07 22:26:36 -0500

    fore

---
+++ jank.py
@@ -0,0 +1,12 @@
+import random
+
+list1 = ["Abby", "Aleisa", "Andrew", "Angie", "BenK", "BenN", "BenW", "Billy", "Brittany", "Caleb", "Corey", "Cyrus", "Dakota", "Des", "Garrett", "Jared", "Jed", "Joshua", "Lida", "Lindsey", "Marian", "Mathew", "Bergy", "Schmidty", "Nathan", "Paul", "Samuel", "Sarah", "Sean", "Sky", "Steve", "Taryn", "Tiera", "Tori"]
+list2 = ["Abby", "Aleisa", "Andrew", "Angie", "BenK", "BenN", "BenW", "Billy", "Brittany", "Caleb", "Corey", "Cyrus", "Dakota", "Des", "Garrett", "Jared", "Jed", "Joshua", "Lida", "Lindsey", "Marian", "Mathew", "Bergy", "Schmidty", "Nathan", "Paul", "Samuel", "Sarah", "Sean", "Sky", "Steve", "Taryn", "Tiera", "Tori"]
+
+for i in range(34):
+    mugger1 = random.choice(list1)
+    mugger2 = random.choice(list2)
+    if mugger1 != mugger2:
+        print("{} {}".format(mugger1, mugger2))
+    list1.remove(mugger1)
+    list2.remove(mugger2)
